535af10685510203cdd6ecc6d81c7b81e6beb005
Sakis Papageorgiou <s.papageorgiou@vieconnect.io>
2019-06-15 21:16:15 +0300

    init

--- app/src/main/java/com/example/trafake/submitActivity.java
+++ app/src/main/java/com/example/trafake/submitActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -43,6 +44,7 @@ public class submitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_submit);
         status = findViewById(R.id.listtext);
 
